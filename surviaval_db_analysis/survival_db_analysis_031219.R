e0ab2595b36e937f4fea5970e54199ded516f156
MM_group <andrea.poletti9@unibo.it>
2019-12-11 14:55:39 +0100

    db analysis

---
+++ surviaval_db_analysis/survival_db_analysis_031219.R
@@ -0,0 +1,159 @@
+library(RODBC) 
+
+db <- odbcConnectAccess2007("C:/Users/mm_gr/Alma Mater Studiorum Università di Bologna/PROJECT SophiaPanel - TP53 - Documenti/TP53_DB_v1.accdb")
+# sqlTables(db)
+df <- sqlFetch(db, "Query_Survival_Analysis")
+
+#________________ data prep _________________________
+
+# create the label for del tp53 call
+
+df$call50_del_TP53 <- ifelse(df$TP53_adj <= 1.5, 1,0)
+table(df$call50_del_TP53, df$Fish_DEL_17)
+
+df$call40_del_TP53 <- ifelse(df$TP53_adj <= 1.6, 1,0)
+table(df$call40_del_TP53, df$Fish_DEL_17)
+
+df$call30_del_TP53 <- ifelse(df$TP53_adj <= 1.7, 1,0)
+table(df$call30_del_TP53, df$Fish_DEL_17)
+
+df$call20_del_TP53 <- ifelse(df$TP53_adj <= 1.8, 1,0)
+table(df$call20_del_TP53, df$Fish_DEL_17)
+
+df$call10_del_TP53 <- ifelse(df$TP53_adj <= 1.9, 1,0)
+table(df$call10_del_TP53, df$Fish_DEL_17)
+
+#============ Survival analysis =====================
+
+library(data.table)
+library(tidyverse)
+library(survival)
+library(survminer)
+
+OS <- Surv( time = df$OS_MESI, event = df$OS_event_death)
+PFS <- Surv( time = df$PFS_I_months, event = df$PFS_I_event)
+
+
+
+#______ t(4;14) ________
+
+ggsurvplot(survfit(OS ~ df$Fish_T_4_14, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$Fish_T_4_14, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$Fish_T_4_14, data = df) %>% summary
+coxph(PFS ~ df$Fish_T_4_14, data = df) %>% summary
+
+#______ t(4;14) ________
+
+ggsurvplot(survfit(OS ~ df$Fish_DEL_17, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$Fish_DEL_17, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$Fish_DEL_17, data = df) %>% summary
+coxph(PFS ~ df$Fish_DEL_17, data = df) %>% summary
+
+df$Fish_DEL_17 %>% table
+
+
+#______ call50_del_TP53 ________
+
+ggsurvplot(survfit(OS ~ df$call50_del_TP53, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$call50_del_TP53, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$call50_del_TP53, data = df) %>% summary
+coxph(PFS ~ df$call50_del_TP53, data = df) %>% summary
+
+#______ call40_del_TP53 ________
+
+ggsurvplot(survfit(OS ~ df$call40_del_TP53, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$call40_del_TP53, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$call40_del_TP53, data = df) %>% summary
+coxph(PFS ~ df$call40_del_TP53, data = df) %>% summary
+
+#______ call30_del_TP53 ________
+
+ggsurvplot(survfit(OS ~ df$call30_del_TP53, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$call30_del_TP53, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$call30_del_TP53, data = df) %>% summary
+coxph(PFS ~ df$call30_del_TP53, data = df) %>% summary
+
+#______ call20_del_TP53 ________
+
+ggsurvplot(survfit(OS ~ df$call20_del_TP53, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$call20_del_TP53, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$call20_del_TP53, data = df) %>% summary
+coxph(PFS ~ df$call20_del_TP53, data = df) %>% summary
+
+#______ call10_del_TP53 ________
+
+ggsurvplot(survfit(OS ~ df$call10_del_TP53, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$call10_del_TP53, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$call10_del_TP53, data = df) %>% summary
+coxph(PFS ~ df$call10_del_TP53, data = df) %>% summary
+
+
+#============== MUT DEL ======================
+
+mutdel <- df %>% filter( TP53_adj < 1.9 & MUT_p53_D_SUB_CLON == 1)
+
+df$mut_del_event <- ifelse(df$TP53_adj < 1.9 & df$MUT_p53_D_SUB_CLON == 1, 1, 0)
+
+
+homdel <- df %>% filter( TP53_adj < 0.6 )
+df$homdel <- ifelse(df$TP53_adj < 0.6, 1, 0)
+
+
+#______ mutdel survival ________
+
+ggsurvplot(survfit(OS ~ df$mut_del_event, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$mut_del_event, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$mut_del_event, data = df) %>% summary
+coxph(PFS ~ df$mut_del_event, data = df) %>% summary
+
+
+table(df$call50_del_TP53, df$Fish_DEL_17)
+
+
+#______ homdel survival ________
+
+ggsurvplot(survfit(OS ~ df$homdel, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$homdel, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$homdel, data = df) %>% summary
+coxph(PFS ~ df$homdel, data = df) %>% summary
+
+df$homdel %>% table
+
+#______ double HIT on TP53 _________
+
+df$doubleHIT <- ifelse(df$mut_del_event ==1 | df$homdel == 1, 1,0)
+df$doubleHIT %>% table
+
+ggsurvplot(survfit(OS ~ df$doubleHIT, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$doubleHIT, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$doubleHIT, data = df) %>% summary
+coxph(PFS ~ df$doubleHIT, data = df) %>% summary
+
+
+table(df$mut_del_event, df$homdel)
+
+
+#______ double HIT with LOH on TP53 _________
+
+df$double_del_LOH <- ifelse(df$mut_del_event ==1 & df$LOH_call == 1, 1,0)
+df$double_del_LOH %>% table
+
+
+ggsurvplot(survfit(OS ~ df$doubleHIT, data = df), pval = T, risk.table = T, xlab = "OS")
+ggsurvplot(survfit(PFS ~ df$doubleHIT, data = df), pval = T, risk.table = T, xlab = "PFS")
+
+coxph(OS ~ df$doubleHIT, data = df) %>% summary
+coxph(PFS ~ df$doubleHIT, data = df) %>% summary
+
+
+table(df$doubleHIT, df$double_del_LOH)
