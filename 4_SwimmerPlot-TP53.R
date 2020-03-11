4ffb912c28286d05c4158b118c4c4f75add3aa16
shirke019 <andrea.poletti9@unibo.it>
2020-03-11 18:04:00 +0100

    New swimmer plot 11/03/2020

--- 4_SwimmerPlot-TP53.R
+++ 4_SwimmerPlot-TP53.R
@@ -93,3 +93,156 @@ data.pts %>% ggplot(aes(UPN, MESI)) +
   theme_bw() +
   theme(panel.grid.minor=element_blank(),
         panel.grid.major=element_blank())
+
+
+
+############################## new DB swimmerplot 11/03/2020 ##################################
+
+library(RODBC)
+
+db <- odbcConnectAccess2007("C:/Users/andre/Alma Mater Studiorum Università di Bologna/PROJECT SophiaPanel - TP53 - Documenti/TP53_DB_v1.accdb")
+
+TABS <- sqlTables(db)$TABLE_NAME
+TABS
+
+# query Diagnosis
+Q_Dia <- sqlFetch(db, "Query_Survival_Analysis")
+# query per tutti i pazienti del DB che hanno i dati molecolari di relapse (65)
+Q_Rel <- sqlFetch(db, "Copia di Query_Survival_Analysis")
+
+
+#=========== data set up diagnosis ========
+
+Q_Dia$flag <- ifelse(Q_Dia$PROTOCOLLO != "BO2005" & Q_Dia$PROTOCOLLO != "EMN02" & 
+                     Q_Dia$TTP_I_months <= 24 & Q_Dia$Prog_I==1 &
+                     Q_Dia$TP53_adj >= 1.9 & Q_Dia$MUT_p53_D_SUB_CLON ==0 , 
+                     1, 0)
+
+Q_Dia$flag %>% table  
+
+DF1 <- Q_Dia %>% filter(flag==0)
+
+DF1$del_TP53_SNP_D <- ifelse(DF1$TP53_adj<1.9,1,0)
+
+DF1$del_TP53_SNP_D %>% table
+
+#=========== data set up relapse ==========
+
+# filtro per i pazienti che sono progrediti (2 eccezioni)
+prog_true <- Q_Rel %>% filter(Prog_I==1)
+# completamento della colonna prog_II: inserimento di 0 nei valori mancanti (pazienti che non sono progrediti ma sono morti o ancora vivi, mancava il dato)
+prog_true$Prog_II[is.na(prog_true$Prog_II)] <- 0
+# calcolo nuova variabile = PFS_2_event
+prog_true$PFS_2_event <- ifelse( prog_true$Prog_II == 1 | prog_true$OS_event_death == 1, 1, 0)
+prog_true$PFS_2_event %>% table
+# convert the dates in correct Date format
+prog_true$data_II_relapse <- as.Date(prog_true$data_II_relapse, origin="1970-01-01")
+prog_true$Date_of_death <- as.Date(prog_true$Date_of_death, origin="1970-01-01")
+prog_true$LAST_FULLOW_UP <- as.Date(prog_true$LAST_FULLOW_UP, origin="1970-01-01")
+prog_true$PFS_I_date <- as.Date(prog_true$PFS_I_date, origin="1970-01-01")
+# calcolo nuova variabile = PFS_2_date
+prog_true$PFS_2_date <- dplyr::if_else(prog_true$Prog_II == 1, 
+                                       as.Date(prog_true$data_II_relapse, origin="1970-01-01"), 
+                                       dplyr::if_else(prog_true$OS_event_death == 1, 
+                                                      as.Date(prog_true$Date_of_death, origin="1970-01-01"), 
+                                                      as.Date(prog_true$LAST_FULLOW_UP,origin="1970-01-01")))
+# calulate the PFS_2_time
+prog_true$PFS_2_time <- prog_true$PFS_2_date - prog_true$PFS_I_date
+# calulate the PFS_2_months
+prog_true$PFS_2_months <- round(prog_true$PFS_2_time / 30.5, 0) 
+# FLAG
+prog_true$flag <- ifelse(prog_true$PROTOCOLLO != "BO2005" & prog_true$PROTOCOLLO != "EMN02" & 
+                           prog_true$TTP_I_months <= 24 & prog_true$Prog_I==1 &
+                           prog_true$TP53_adj_D >= 1.9 & prog_true$MUT_p53_D_SUB_CLON ==0 , 
+                         1, 0)
+
+DF2 <- prog_true %>% filter(flag==0)
+
+DF2$del_TP53_SNP_R <- ifelse(DF2$TP53_adj_R < 1.9, 1, 0)
+
+
+
+#============== creation of databse for swimmer plot ===============
+
+data_D <- DF1 %>% select(UPN, MUT_p53_D_SUB_CLON, del_TP53_SNP_D, PFS_I_months, PFS_I_event, OS_MESI, OS_event_death)
+
+data_R <- DF2 %>% select(UPN, MUT_P53_R_SUB_CLON, del_TP53_SNP_R, PFS_2_event, PFS_2_months )
+
+pts_all <- full_join(data_D, data_R, by = "UPN") %>% 
+  select(UPN, MUT_p53_D_SUB_CLON, MUT_P53_R_SUB_CLON, 
+         del_TP53_SNP_D, del_TP53_SNP_R, 
+         PFS_I_months:OS_event_death, PFS_2_event,PFS_2_months) %>% 
+  filter(UPN != "3581") #remove pts UPN 3581 because unvalutable SNP purity at Diagnosis (SNP218) missing data
+
+
+
+pts_swimmer <- pts_all %>% filter(MUT_p53_D_SUB_CLON==1 | MUT_P53_R_SUB_CLON==1 | del_TP53_SNP_D ==1 | del_TP53_SNP_R==1) 
+
+pts_swimmer$Second_PFS_months <- pts_swimmer$PFS_I_months + pts_swimmer$PFS_2_months
+
+pts_swimmer.m = melt(pts_swimmer %>% select(UPN, OS_MESI, PFS_I_months, MUT_p53_D_SUB_CLON, MUT_P53_R_SUB_CLON, del_TP53_SNP_D, del_TP53_SNP_R),
+             id.var=c("UPN","OS_MESI","PFS_I_months"))
+
+
+pts_swimmer.m.D = melt(pts_swimmer %>% select(UPN, MUT_p53_D_SUB_CLON, del_TP53_SNP_D),
+                       id.var=c("UPN"))
+pts_swimmer.m.D$value[pts_swimmer.m.D$value==0] <- NA
+
+
+pts_swimmer.m.R = melt(pts_swimmer %>% select(UPN, PFS_I_months, MUT_P53_R_SUB_CLON, del_TP53_SNP_R),
+                       id.var=c("UPN", "PFS_I_months"))
+pts_swimmer.m.R$valuetime <- ifelse(pts_swimmer.m.R$value == 1, pts_swimmer.m.R$PFS_I_months, NA)
+
+
+# pts_swimmer.alive = pts_swimmer %>% select(UPN, OS_event_death, OS_MESI)
+
+ORDER <- pts_swimmer$OS_MESI %>% order()
+
+pts_swimmer$UPN <- factor(pts_swimmer$UPN, levels = pts_swimmer$UPN[ORDER], ordered = T)
+
+pts_swimmer %>% ggplot(aes(UPN, OS_MESI)) +
+  
+  geom_bar(stat="identity", colour="black", fill="grey90", width=0.8)+
+  
+  # alterations D
+  geom_point(data=pts_swimmer.m.D, 
+             aes(UPN, -value, colour=variable, shape=variable), size=4) +
+  
+  # alterations R
+  geom_point(data=pts_swimmer.m.R, 
+             aes(UPN, valuetime, colour=variable, shape=variable), size=4) +
+  
+  # deaths
+  geom_point(data=pts_swimmer %>% filter(OS_event_death==1),
+             aes(UPN, OS_MESI+1), size=2.5, shape=4) +
+  
+  # alive
+  geom_segment(data=pts_swimmer %>% filter(OS_event_death==0),
+               aes(x=UPN, xend=UPN, y=OS_MESI + 0.5, yend=OS_MESI + 2),
+               pch=25, size=0.5, arrow=arrow(type="closed", length=unit(0.07,"in"))) +
+  
+  # relapsed with EVAL
+  geom_point(data=pts_swimmer %>% filter(PFS_I_event==1 & !is.na(MUT_P53_R_SUB_CLON) & !is.na(del_TP53_SNP_R)),
+               aes(x=UPN, y= PFS_I_months-0.8), shape=124, size=3) +
+  
+  # relapsed no EVAL
+  geom_point(data=pts_swimmer %>% filter(PFS_I_event==1 & (is.na(MUT_P53_R_SUB_CLON) | is.na(del_TP53_SNP_R))),
+             aes(x=UPN, y= PFS_I_months-0.8), shape=1, size=3) +
+  
+  # SECOND relapsed no EVAL
+  geom_point(data=pts_swimmer %>% filter(PFS_2_event==1),
+             aes(x=UPN, y= Second_PFS_months-0.8), shape=1, size=3) +
+  
+  
+  coord_flip() +
+  
+  scale_shape_manual(values = c(20,20,8,8) ) +
+  # scale_fill_manual(values=hcl(seq(15,375,length.out=5)[1:4],100,70)) +
+  # scale_colour_manual(values=c(hcl(seq(15,375,length.out=3)[1:2],100,40),"black")) +
+  scale_y_continuous(limits=c(-1,172), breaks=seq(0,180,10), expand = expand_scale(mult = c(0.01,0))) +
+  
+  labs(y="Months") +
+  theme_bw() +
+  theme(panel.grid.minor=element_blank(),
+        panel.grid.major=element_blank())
+
